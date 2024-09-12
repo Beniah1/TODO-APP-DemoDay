7c1bd06181d9bb530ab30ecb5f402d34883f19ad
Beniah1 <diallobeniah@gmail.com>
2024-09-12 15:56:39 +0000

    feat: add initial files for Todo App components and configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css';
+
+import Todo from './components/Todo'
+
+const App = () => (
+  <div className='bg-stone-900 text-white grid py-4 min-h-screen'>
+    <Todo/> 
+  </div>
+)
+
+export default App

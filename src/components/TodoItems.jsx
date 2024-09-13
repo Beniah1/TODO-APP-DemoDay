8c2dc9bd7a3ce894c0063b7d51bf57d6d19d7bcd
Beniah1 <diallobeniah@gmail.com>
2024-09-13 13:15:14 +0000

    fix(todoItems): apply line-through effect to completed items

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import tick from "../assets/tick.png";
 import not_tick from "../assets/not_tick.png";
 import delete_icon from "../assets/delete.png";
-const TodoItems = ({text, id, isComplete, deleteTodo, toggle}) => {
+const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
   return (
     <div onClick={() => toggle(id)} className="flex item-center my-3 gap-2">
       <div className="flex flex-I items-center cursor-pointer">
-        <img src={tick} alt="" className="w-7" />
-        <p className="tex-s1ate-700 ml-4 text-[17] text-black">{text}</p>
+        <img src={isComplete ? tick : not_tick} alt="" className="w-7" />
+        <p className={`text-slate-700 ml-4 text-[17] text-black ${isComplete ? "line-through" : ""}`}>{text}</p>
       </div>
       <img
         onClick={() => deleteTodo(id)}
         src={delete_icon}
         alt=""
         className="w-4 h-5 cursor-pointer ml-auto"
-      />{" "}
+      />
     </div>
   );
 };
